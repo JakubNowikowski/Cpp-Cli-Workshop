5c15b443baf1d91da03b815426c007b4467ab58f
Jakub Nowikowski <kubanowikowski@gmail.com>
2019-06-21 12:07:03 +0200

    files added

---
+++ cliWorkshops/cliWorkshops/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CliNamespace;
+
+namespace cliWorkshops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ManagedClass mc = new CliNamespace.ManagedClass();
+            int[] sortedArray = { 4, 3, 8, 5 };
+
+            foreach(int e in sortedArray)
+                Console.WriteLine(e);
+
+            Console.WriteLine("*************");
+
+            mc.insertion_sort(sortedArray);
+
+            foreach (int e in sortedArray)
+                Console.WriteLine(e);
+
+            Console.ReadKey();
+        }
+    }
+}
